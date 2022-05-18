7ed93430c1587474f1d5f74b60ab05d568b163bf
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-05-18 16:16:01 +0300

    Create Stacks-and-Queues.py

---
+++ Python-Advanced/Stacks-and-Queues.py
@@ -0,0 +1,41 @@
+string = list(input())
+answer = ''
+
+while string:
+    char = string.pop()
+    answer += char
+
+print(answer)
+
+#--------------------------------------------------------------------------------------------
+
+expression = input()
+stack = []
+
+for index in range(len(expression)):
+    if expression[index] == '(':
+        stack.append(index)
+    elif expression[index] == ')':
+        bracket = stack.pop()
+        print(expression[bracket:index + 1])
+
+#--------------------------------------------------------------------------------------------
+
+from collections import deque
+
+q = deque()
+
+while True:
+    command = input()
+    if command == 'Paid':
+        while q:
+            print(q.popleft())
+    elif command == 'End':
+        print(f"{len(q)} people remaining.")
+        break
+    else:
+        q.append(command)
+
+#--------------------------------------------------------------------------------------------
+
+
