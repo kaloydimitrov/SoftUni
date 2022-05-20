5fc4d2b07838087a0ab80fdf328de256b7909d6c
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-05-20 13:08:56 +0300

    daily exercises

--- Python-Advanced/Stacks-and-Queues.py
+++ Python-Advanced/Stacks-and-Queues.py
@@ -38,3 +38,51 @@ while True:
 
 # --------------------------------------------------------------------------------------------
 
+from collections import deque
+
+water = int(input())
+people = deque()
+
+command = input()
+
+while command != "Start":
+    people.append(command)
+    command = input()
+
+second_command = input()
+
+while second_command != "End":
+    if "refill" in second_command:
+        water += int(second_command.split()[1])
+    else:
+        person = people.popleft()
+        if int(second_command) > water:
+            print(f"{person} must wait")
+        else:
+            print(f"{person} got water")
+            water -= int(second_command)
+
+    second_command = input()
+
+print(str(water) + " liters left")
+
+# --------------------------------------------------------------------------------------------
+
+from collections import deque
+
+kids = deque(input().split())
+
+toss = int(input())
+counter = 0
+
+while len(kids) > 1:
+    kid = kids.popleft()
+    counter += 1
+
+    if counter == toss:
+        counter = 0
+        print(f"Removed {kid}")
+    else:
+        kids.append(kid)
+
+print("Last is " + kids[0])
