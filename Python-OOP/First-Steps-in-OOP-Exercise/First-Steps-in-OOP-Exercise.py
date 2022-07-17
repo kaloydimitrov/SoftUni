877483a1e7eafbe8647125e65ceff285f30c254d
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-07-17 17:10:19 +0300

    projects I did today

---
+++ Python-OOP/First-Steps-in-OOP-Exercise/First-Steps-in-OOP-Exercise.py
@@ -0,0 +1,128 @@
+class Shop:
+    def __init__(self, name, items):
+        self.name = name
+        self.items = items
+
+    def get_items_count(self):
+        return len(self.items)
+
+
+# ---------------------------------------------------------------------------
+
+class Hero:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def defend(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            self.health = 0
+            return f"{self.name} was defeated"
+
+    def heal(self, amount):
+        self.health += amount
+
+# ---------------------------------------------------------------------------
+
+
+class Employee:
+    def __init__(self, id, first_name, last_name, salary):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_annual_salary(self):
+        self.salary *= 12
+        return self.salary
+
+    def raise_salary(self, amount):
+        self.salary += amount
+        return self.salary
+
+# ---------------------------------------------------------------------------
+
+
+class Cup:
+    def __init__(self, size, quantity):
+        self.size = size
+        self.quantity = quantity
+
+    def fill(self, quantity):
+        if self.size - self.quantity > quantity:
+            self.quantity += quantity
+
+    def status(self):
+        return self.size - self.quantity
+
+# ---------------------------------------------------------------------------
+
+
+class Flower:
+    def __init__(self, name, water_requirements):
+        self.name = name
+        self.water_requirements = water_requirements
+        self.is_happy = False
+
+    def water(self, quantity):
+        if quantity >= self.water_requirements:
+            self.is_happy = True
+
+    def status(self):
+        if self.is_happy:
+            return f"{self.name} is happy"
+        else:
+            return f"{self.name} is not happy"
+
+# ---------------------------------------------------------------------------
+
+
+class SteamUser:
+    def __init__(self, username: str, games: list):
+        self.username = username
+        self.games = games
+        self.played_hours = 0
+
+    def play(self, game, hours):
+        if game in self.games:
+            self.played_hours += hours
+            return f"{self.username} is playing {game}"
+        return f"{game} is not in library"
+
+    def buy_game(self, game):
+        if game not in self.games:
+            self.games.append(game)
+            return f"{self.username} bought {game}"
+        return f"{game} is already in your library"
+
+    def status(self):
+        return f"{self.username} has {len(self.games)} games. Total play time: {self.played_hours}"
+
+# ---------------------------------------------------------------------------
+
+
+class Programmer:
+    def __init__(self, name, language, skills):
+        self.name = name
+        self.language = language
+        self.skills = skills
+
+    def watch_course(self, course_name, language, skills_earned):
+        if language == self.language:
+            self.skills += skills_earned
+            return f"{self.name} watched {course_name}"
+        return f"{self.name} does not know {language}"
+
+    def change_language(self, new_language, skills_needed):
+        if self.skills >= skills_needed and new_language != self.language:
+            previous_language = self.language
+            self.language = new_language
+            return f"{self.name} switched from {previous_language} to {new_language}"
+        elif self.skills >= skills_needed and new_language == self.language:
+            return f"{self.name} already knows {new_language}"
+
+        return f"{self.name} needs {skills_needed - self.skills} more skills"
---
+++ Python-OOP/First-Steps-in-OOP-Exercise/Pokemon-Battle/project/trainer.py
@@ -0,0 +1,29 @@
+from project.pokemon import Pokemon
+
+
+class Trainer:
+    def __init__(self, name):
+        self.name = name
+        self.pokemons = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        if pokemon.name in [x.name for x in self.pokemons]:
+            return "This pokemon is already caught"
+        self.pokemons.append(pokemon)
+        return f"Caught {pokemon.name} with health {pokemon.health}"
+
+    def release_pokemon(self, pokemon_name):
+        if pokemon_name not in [x.name for x in self.pokemons]:
+            return "Pokemon is not caught"
+        for pokemon in self.pokemons:
+            if pokemon.name == pokemon_name:
+                self.pokemons.remove(pokemon)
+                break
+        return f"You have released {pokemon_name}"
+
+    def trainer_data(self):
+        result = f"Pokemon Trainer {self.name}\nPokemon count {len(self.pokemons)}"
+        for name_health in self.pokemons:
+            result += f"\n- {name_health.name} with health {name_health.health}"
+
+        return result
