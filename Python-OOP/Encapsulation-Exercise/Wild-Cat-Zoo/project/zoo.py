1085a1ca3f5aab0ca7c9be159194a25e08bd6e45
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-07-19 20:10:46 +0300

    the biggest project I've done so far

---
+++ Python-OOP/Encapsulation-Exercise/Wild-Cat-Zoo/project/zoo.py
@@ -0,0 +1,103 @@
+from project.caretaker import Caretaker
+from project.keeper import Keeper
+from project.cheetah import Cheetah
+from project.lion import Lion
+from project.animal import Animal
+from project.worker import Worker
+from project.tiger import Tiger
+from project.vet import Vet
+
+
+class Zoo:
+    def __init__(self, name, budget, animal_capacity, workers_capacity):
+        self.name = name
+        self.__budget = budget
+        self.__animal_capacity = animal_capacity
+        self.__workers_capacity = workers_capacity
+
+        self.animals = []
+        self.workers = []
+
+    def add_animal(self, animal: Animal, price):
+        if self.__budget >= price and len(self.animals) < self.__animal_capacity:
+            self.animals.append(animal)
+            self.__budget -= price
+            return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
+
+        elif len(self.animals) < self.__animal_capacity:
+            return "Not enough budget"
+
+        else:
+            return "Not enough space for animal"
+
+    def hire_worker(self, worker: Worker):
+        if len(self.workers) < self.__workers_capacity:
+            self.workers.append(worker)
+            return f"{worker.name} the {worker.__class__.__name__} hired successfully"
+        return "Not enough space for worker"
+
+    def fire_worker(self, worker_name):
+        for worker in self.workers:
+            if worker.name == worker_name:
+                self.workers.remove(worker)
+                return f"{worker_name} fired successfully"
+        return f"There is no {worker_name} in the zoo"
+
+    def pay_workers(self):
+        salaries = 0
+        for worker in self.workers:
+            salaries += worker.salary
+        if self.__budget >= salaries:
+            self.__budget -= salaries
+            return f"You payed your workers. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to pay your workers. They are unhappy"
+
+    def tend_animals(self):
+        total_care_money = 0
+        for animal in self.animals:
+            total_care_money += animal.money_for_care
+        if self.__budget >= total_care_money:
+            self.__budget -= total_care_money
+            return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to tend the animals. They are unhappy."
+
+    def profit(self, amount):
+        self.__budget += amount
+
+    def animals_status(self):
+        result = ""
+        lions = [repr(a) + "\n" for a in self.animals if isinstance(a, Lion)]
+        tigers = [repr(a) + "\n" for a in self.animals if isinstance(a, Tiger)]
+        cheetahs = [repr(a) + "\n" for a in self.animals if isinstance(a, Cheetah)]
+
+        result += f"You have {len(self.animals)} animals\n"
+
+        result += f"----- {len(lions)} Lions:\n"
+        result += "".join(lions)
+
+        result += f"----- {len(tigers)} Tigers:\n"
+        result += "".join(tigers)
+
+        result += f"----- {len(cheetahs)} Cheetahs:\n"
+        result += "".join(cheetahs)
+
+        return result.strip()
+
+    def workers_status(self):
+        result = ""
+        keepers = [repr(a) + "\n" for a in self.workers if isinstance(a, Keeper)]
+        caretakers = [repr(a) + "\n" for a in self.workers if isinstance(a, Caretaker)]
+        vets = [repr(a) + "\n" for a in self.workers if isinstance(a, Vet)]
+
+        result += f"You have {len(self.workers)} workers\n"
+
+        result += f"----- {len(keepers)} Keepers:\n"
+        result += "".join(keepers)
+
+        result += f"----- {len(caretakers)} Caretakers:\n"
+        result += "".join(caretakers)
+
+        result += f"----- {len(vets)} Vets:\n"
+        result += "".join(vets)
+
+        return result.strip()
--- Python-OOP/Encapsulation/Encapsulation.py
+++ Python-OOP/Encapsulation/Encapsulation.py
@@ -12,3 +12,94 @@ class Person:
 # ----------------------------------------------------------------------------------
 
 
+class Mammal:
+    __kingdom = "animals"
+
+    def __init__(self, name, type, sound):
+        self.name = name
+        self.type = type
+        self.sound = sound
+
+    def make_sound(self):
+        return f"{self.name} makes {self.sound}"
+
+    def get_kingdom(self):
+        return Mammal.__kingdom
+
+    def info(self):
+        return f"{self.name} is of type {self.type}"
+
+# ----------------------------------------------------------------------------------
+
+
+class Profile:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        if not (5 <= len(value) <= 15):
+            raise ValueError("The username must be between 5 and 15 characters.")
+        self.__username = value
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        if not (len(value) >= 8 and any([x.isupper() for x in value]) and any([x.isdigit() for x in value])):
+            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+        self.__password = value
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.password)}'
+
+# ----------------------------------------------------------------------------------
+
+
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name):
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail):
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        email_data = email.split("@")
+        username = email_data[0]
+        mail, domain = email_data[1].split(".")
+        return self.__is_name_valid(username) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain)
+
+# ----------------------------------------------------------------------------------
+
+
+class Account:
+    def __init__(self, id, balance, pin):
+        self.__id = id
+        self.balance = balance
+        self.__pin = pin
+
+    def get_id(self, pin):
+        if self.__pin == pin:
+            return self.__id
+        return "Wrong pin"
+
+    def change_pin(self, old_pin, new_pin):
+        if self.__pin == old_pin:
+            self.__pin = new_pin
+            return "Pin changed"
+        return "Wrong pin"
