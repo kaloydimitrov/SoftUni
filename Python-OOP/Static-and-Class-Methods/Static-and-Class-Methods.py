c926ed9765c66d48ff98bef27a387d99e9c8b7c4
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-07-23 14:16:11 +0300

    dailies

--- Python-OOP/Static-and-Class-Methods/Static-and-Class-Methods.py
+++ Python-OOP/Static-and-Class-Methods/Static-and-Class-Methods.py
@@ -50,3 +50,48 @@ class Shop:
     def __repr__(self):
         return f"{self.name} of type {self.type} with capacity {self.capacity}"
 
+# --------------------------------------------------------------------------
+
+
+import math
+
+
+def roman_to_int(s):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
+             'CD': 400, 'CM': 900}
+    i = 0
+    num = 0
+    while i < len(s):
+        if i + 1 < len(s) and s[i:i + 2] in roman:
+            num += roman[s[i:i + 2]]
+            i += 2
+        else:
+            num += roman[s[i]]
+            i += 1
+    return num
+
+
+class Integer:
+    def __init__(self, value):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value: str):
+        if not isinstance(float_value, float):
+            return "value is not a float"
+        return cls(math.floor(float(float_value)))
+
+    @classmethod
+    def from_roman(cls, value: str):
+        return cls(roman_to_int(value))
+
+    @classmethod
+    def from_string(cls, value):
+        if not isinstance(value, str):
+            return "wrong type"
+        return cls(int(value))
+
+# --------------------------------------------------------------------------
+
+
+
