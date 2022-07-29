19e740556e55e55a26e533ca8787341b4d0eaf15
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-07-29 21:55:39 +0300

    dailies

--- Python-OOP/Polymorphism-and-Abstraction-Exercise/Polymorphism-and-Abstraction-Exercise.py
+++ Python-OOP/Polymorphism-and-Abstraction-Exercise/Polymorphism-and-Abstraction-Exercise.py
@@ -37,3 +37,36 @@ class Truck(Vehicle):
 # ---------------------------------------------------------------------------------
 
 
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+    def __add__(self, other):
+        return Person(self.name, other.surname)
+
+
+class Group:
+    def __init__(self, name: str, people: list):
+        self.name = name
+        self.people = people
+
+    def __len__(self):
+        return len(self.people)
+
+    def __add__(self, other):
+        return Group(f"{self.name} {other.name}", self.people + other.people)
+
+    def __str__(self):
+        return f"Group {self.name} with members {', '.join([str(p) for p in self.people])}"
+
+    def __getitem__(self, item):
+        return f"Person {item}: {str(self.people[item])}"
+
+
+# ---------------------------------------------------------------------------------
+
+
