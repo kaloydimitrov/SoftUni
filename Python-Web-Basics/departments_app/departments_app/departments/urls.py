e389b33b1ac4f9bb8359729a0cccba39748277ec
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-11-24 23:50:29 +0200

    adding more advanced views

--- Python-Web-Basics/departments_app/departments_app/departments/urls.py
+++ Python-Web-Basics/departments_app/departments_app/departments/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from departments_app.departments.views import home, department, department_details
+from departments_app.departments.views import home, department, department_details, department_details_html
 
 
 urlpatterns = (
     path('', home),
     path('home/', home),
     path('department/', department),
-    path('department/<int:department_id>/', department_details)
+    path('department/<int:department_id>/', department_details_html)
 )
--- Python-Web-Basics/departments_app/departments_app/departments/views.py
+++ Python-Web-Basics/departments_app/departments_app/departments/views.py
@@ -3,12 +3,27 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('This is home')
+    return HttpResponse(f'{request.method}: This is home')
 
 
 def department(request):
-    return HttpResponse('This is the default department')
+    return HttpResponse(f'{request.method}: This is department (default)')
 
 
 def department_details(request, department_id):
-    return HttpResponse(f'This is department {department_id}')
+    return HttpResponse(f'{request.method}: This is department {department_id}')
+
+
+def department_details_html(request, department_id):
+    department_name = ''
+
+    if department_id == 1:
+        department_name = 'Developers'
+    elif department_id == 2:
+        department_name = 'Trainers'
+
+    html = '<html><body><h1>' \
+           f'Department name: {department_name}, Department ID {department_id}' \
+           '</html></body></h1>'
+
+    return HttpResponse(html)
