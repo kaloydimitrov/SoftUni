06fbb50a19e26dc332ea9808c1d9b4e74bb61d2a
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-12-01 23:40:52 +0200

    learning migrations

--- Python-Web-Basics/models_demo/models_demo/web_demo/models.py
+++ Python-Web-Basics/models_demo/models_demo/web_demo/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 
 class Employee(models.Model):
-    first_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    email_address = models.EmailField()
+
