7bcfa5fdd8c205f27ac13e68cffcb37a18b5c53c
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-12-02 01:08:49 +0200

    adding superuser + more migrations

--- Python-Web-Basics/models_demo/models_demo/web_demo/admin.py
+++ Python-Web-Basics/models_demo/models_demo/web_demo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from models_demo.web_demo.models import Employee
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
--- Python-Web-Basics/models_demo/models_demo/web_demo/models.py
+++ Python-Web-Basics/models_demo/models_demo/web_demo/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Employee(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    age = models.IntegerField(null=True)
     email_address = models.EmailField()
-
+    address = models.TextField(null=True)
+    salary = models.IntegerField(null=True)
