c49274298e4c2430bec14313aed9d72e3c0ad58b
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-12-08 01:11:12 +0200

    working on models

--- Python-Web-Basics/models_two/models_two/web/admin.py
+++ Python-Web-Basics/models_two/models_two/web/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from models_two.web.models import Employees
+from models_two.web.models import Employees, Departments
 
 
-admin.site.register(Employees)
+@admin.register(Employees)
+class EmployeesAdmin(admin.ModelAdmin):
+    pass
+# admin.site.register(Employees)
+
+
+@admin.register(Departments)
+class DepartmentsAdmin(admin.ModelAdmin):
+    pass
+# admin.site.register(Departments)
--- Python-Web-Basics/models_two/models_two/web/models.py
+++ Python-Web-Basics/models_two/models_two/web/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+
+class Departments(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
+
+
 LEVELS = (
-        'Trainee',
-        'Junior',
-        'Middle',
-        'Senior'
-    )
+    'Trainee',
+    'Junior',
+    'Middle',
+    'Senior'
+)
 
 
 class Employees(models.Model):
@@ -17,6 +25,12 @@ class Employees(models.Model):
     level = models.IntegerField(choices=((l, LEVELS[l]) for l in range(len(LEVELS))), default='Trainee')
     salary = models.IntegerField()
     works_full_time = models.BooleanField(default=True)
+
+    department = models.ForeignKey(
+        Departments,
+        on_delete=models.CASCADE,  # .SET_NULL  .RESTRICT
+        null=True, blank=True,
+    )
 
     def __str__(self):
         return f"{self.id} - {self.first_name} {self.last_name}"
