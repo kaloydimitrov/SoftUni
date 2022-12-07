c1339ace453e035473861a3b05594eb001f2d770
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-12-07 23:53:33 +0200

    still working on models

--- Python-Web-Basics/models_two/models_two/web/models.py
+++ Python-Web-Basics/models_two/models_two/web/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
+LEVELS = (
+        'Trainee',
+        'Junior',
+        'Middle',
+        'Senior'
+    )
+
 
 class Employees(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField(null=True, blank=True)
+    age = models.IntegerField()
     email = models.EmailField()
-    address = models.TextField(null=True)
-    salary = models.IntegerField(null=True)
+    address = models.TextField(null=True, blank=True)
+    level = models.IntegerField(choices=((l, LEVELS[l]) for l in range(len(LEVELS))), default='Trainee')
+    salary = models.IntegerField()
+    works_full_time = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.id} - {self.first_name} {self.last_name}"
