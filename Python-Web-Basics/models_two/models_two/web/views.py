b6acca965be0ba1e89be5d902650523c3a644da5
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-12-08 23:51:24 +0200

    working on models

--- Python-Web-Basics/models_two/models_two/web/views.py
+++ Python-Web-Basics/models_two/models_two/web/views.py
@@ -10,6 +10,9 @@ def home(request):
 def employee_details(request):
     context = {
         "employees": Employees.objects.all(),
+        "employees_filter": Employees.objects.order_by('first_name', 'last_name'),
+        "employees_pk": Employees.objects.get(pk=1),  # Employees.objects.get(department_id=3)
+        "employee_department_name": Employees.objects.filter(department__name='Embedded Systems')
     }
 
     return render(request, 'index.html', context)
