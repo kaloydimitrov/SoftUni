03f4efb0deee75bc82f9a04da327cfeb96093265
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-12-15 23:48:17 +0200

    form to db

--- Python-Web-Basics/forms_demo/forms_demo/web/views.py
+++ Python-Web-Basics/forms_demo/forms_demo/web/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django import forms
 from django.shortcuts import render
+from forms_demo.web.models import Person
 
 
 def home(request):
@@ -13,13 +14,20 @@ class FirstForm(forms.Form):
 
 
 def index(request):
+    name = None
     if request.method == "GET":
         form = FirstForm
     else:
         form = FirstForm(request.POST)
+        form.is_valid()
+        name = form.cleaned_data['your_name']
+        Person.objects.create(
+            name=name,
+        )
 
     context = {
         "form": FirstForm,
+        "name": name,
     }
 
     return render(request, 'index.html', context)
