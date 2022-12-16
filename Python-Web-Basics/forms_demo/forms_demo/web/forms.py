87357c7bb3f0a7c94b0108c5f111e50eceb70e14
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2022-12-17 01:07:43 +0200

    adding some more functionality

--- Python-Web-Basics/forms_demo/forms_demo/web/forms.py
+++ Python-Web-Basics/forms_demo/forms_demo/web/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class FirstForm(forms.Form):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
-    age = forms.IntegerField(min_value=5)
+    age = forms.IntegerField(min_value=5, initial=0)
     sex = forms.ChoiceField(
         required=False,
         label='Gender',
@@ -12,6 +12,18 @@ class FirstForm(forms.Form):
             (0, 'None'),
             (1, 'Male'),
             (2, 'Female'),
-            (3, "I'd rather not to give")
-        ),
-    )
+            (3, "I'd rather not to give"),
+        ))
+    address = forms.CharField(max_length=300, widget=forms.Textarea(
+        attrs={
+            'cols': 20, 'rows': 5,
+            'class': 'special',
+            'title': 'add comment'
+        }))
+    radio_buttons = forms.ChoiceField(
+        choices=(
+            (0, 'None'),
+            (1, 'One'),
+            (2, 'Two'),
+            (3, 'Three'),
+        ))
--- Python-Web-Basics/forms_demo/forms_demo/web/models.py
+++ Python-Web-Basics/forms_demo/forms_demo/web/models.py
@@ -3,5 +3,3 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=30)
-    age = models.IntegerField()
-    gay = models.BooleanField()
