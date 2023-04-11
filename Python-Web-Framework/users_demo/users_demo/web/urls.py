7733d40916ef5a72ad60ccee1416c6d3e38400eb
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2023-04-11 20:31:47 +0300

    making some updates on the user side

--- Python-Web-Framework/users_demo/users_demo/web/urls.py
+++ Python-Web-Framework/users_demo/users_demo/web/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from users_demo.web.views import index, UserRegister
+from users_demo.web.views import index, UserRegister, UserLogin, UserDetails
 
 urlpatterns = (
     path('', index, name='home'),
-    path('register/', UserRegister.as_view(), name='register')
+    path('register/', UserRegister.as_view(), name='register'),
+    path('login/', UserLogin.as_view(), name='login'),
+    path('user-info/<int:pk>/', UserDetails.as_view(), name='user-details')
 )
--- Python-Web-Framework/users_demo/users_demo/web/views.py
+++ Python-Web-Framework/users_demo/users_demo/web/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.models import User
-from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
+
+
+def base(request):
+    context = {
+        'user': request.user
+    }
+
+    return render(request, 'base/base.html', context)
 
 
 def index(request):
@@ -12,3 +22,14 @@ class UserRegister(CreateView):
     form_class = UserCreationForm
     template_name = 'user_register.html'
     context_object_name = 'form'
+    success_url = reverse_lazy('home')
+
+
+class UserLogin(LoginView):
+    template_name = 'user_login.html'
+    success_url = reverse_lazy('home')
+
+
+class UserDetails(DetailView):
+    template_name = 'user_details.html'
+    model = User
