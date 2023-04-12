62c042492d6e6e392224df91a0b91bec5cf86eab
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2023-04-12 13:55:26 +0300

    creating a logout

--- Python-Web-Framework/users_demo/users_demo/web/urls.py
+++ Python-Web-Framework/users_demo/users_demo/web/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from users_demo.web.views import index, UserRegister, UserLogin, UserDetails
+from users_demo.web.views import index, UserRegister, UserLogIn, UserLogOut, UserDetails
 
 urlpatterns = (
     path('', index, name='home'),
     path('register/', UserRegister.as_view(), name='register'),
-    path('login/', UserLogin.as_view(), name='login'),
+    path('login/', UserLogIn.as_view(), name='login'),
+    path('logout/', UserLogOut.as_view(), name='logout'),
     path('user-info/<int:pk>/', UserDetails.as_view(), name='user-details')
 )
--- Python-Web-Framework/users_demo/users_demo/web/views.py
+++ Python-Web-Framework/users_demo/users_demo/web/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
+from django import forms
 
 
 def base(request):
@@ -18,16 +19,38 @@ def index(request):
     return render(request, 'index.html')
 
 
+class MyUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email')
+        labels = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name'
+        }
+
+
 class UserRegister(CreateView):
-    form_class = UserCreationForm
+    form_class = MyUserCreationForm
     template_name = 'user_register.html'
     context_object_name = 'form'
     success_url = reverse_lazy('home')
 
 
-class UserLogin(LoginView):
+class UserLogIn(LoginView):
     template_name = 'user_login.html'
-    success_url = reverse_lazy('home')
+    next_page = reverse_lazy('home')
+
+
+class UserLogOut(LogoutView):
+    next_page = reverse_lazy('home')
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        return super().dispatch(request, *args, **kwargs)
 
 
 class UserDetails(DetailView):
