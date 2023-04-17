16da6bbab1e56ef6f6fed24952bd46402d7e5772
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2023-04-18 01:20:04 +0300

    updates on html, css, models

--- Python-Web-Framework/users_demo/users_demo/web/models.py
+++ Python-Web-Framework/users_demo/users_demo/web/models.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.db import models
 
 
@@ -5,12 +6,26 @@ class Pizza(models.Model):
     SIZE_CHOICES = [
         ('sm', 'Small'),
         ('me', 'Medium'),
-        ('la', 'Large'), ]
+        ('la', 'Large'),
+    ]
 
     name = models.CharField(max_length=30)
     ingredients = models.TextField()
     photo = models.ImageField(upload_to='static/images')
-    size = models.CharField(choices=SIZE_CHOICES, max_length=30, default='Medium')
+    size = models.CharField(
+        choices=SIZE_CHOICES,
+        max_length=30,
+        default='me',
+    )
 
     def __str__(self):
         return f"{self.name}"
+
+
+class PizzaForm(forms.ModelForm):
+    class Meta:
+        model = Pizza
+        fields = ['name', 'ingredients', 'photo', 'size']
+        widgets = {
+            'size': forms.RadioSelect(),
+        }
