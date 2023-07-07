d1f9503259000e7932e777fa6e4e673191b40cec
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2023-07-07 22:09:08 +0300

    req.txt

--- Exams/Python-Web-Framework-Retake-Exam-20-August-2023/PizzaGang/PizzaGang/main/filters.py
+++ Exams/Python-Web-Framework-Retake-Exam-20-August-2023/PizzaGang/PizzaGang/main/filters.py
@@ -1,9 +1,13 @@
 import django_filters
+from django_filters import CharFilter
 
 from .models import Pizza
 
 
 class PizzaOrderFilter(django_filters.FilterSet):
+    name = CharFilter(field_name='name', lookup_expr='icontains')
+    ingredients = CharFilter(field_name='ingredients', lookup_expr='icontains')
+
     class Meta:
         model = Pizza
-        fields = ('name', 'ingredients', 'price', 'is_special')
+        fields = ('name', 'ingredients', 'price', 'is_special', 'is_offer', 'is_vege')
