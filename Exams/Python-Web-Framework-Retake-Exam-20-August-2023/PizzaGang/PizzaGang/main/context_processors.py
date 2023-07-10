f08bd864481e51bc9e884a837f160c22ffb452da
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2023-07-10 22:24:02 +0300

    context processor fix, no cart when no user

--- Exams/Python-Web-Framework-Retake-Exam-20-August-2023/PizzaGang/PizzaGang/main/context_processors.py
+++ Exams/Python-Web-Framework-Retake-Exam-20-August-2023/PizzaGang/PizzaGang/main/context_processors.py
@@ -2,10 +2,12 @@ from .models import Cart, CartItem
 
 
 def get_cart_items_count(request):
-    user = request.user
-    cart = Cart.objects.get(user=user)
+    cart_items_count = 0
 
-    cart_items = CartItem.objects.filter(cart=cart)
-    cart_items_count = cart_items.count()
+    if request.user.is_authenticated:
+        user = request.user
+        cart = Cart.objects.get(user=user)
+        cart_items = CartItem.objects.filter(cart=cart)
+        cart_items_count = cart_items.count()
 
     return {'cart_items_count': cart_items_count}
