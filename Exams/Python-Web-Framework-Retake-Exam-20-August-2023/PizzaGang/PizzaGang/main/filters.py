0b9aa6b6a0638e5a0393000d917da2f0751b3502
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2023-07-11 19:35:04 +0300

    filter, design changes

--- Exams/Python-Web-Framework-Retake-Exam-20-August-2023/PizzaGang/PizzaGang/main/filters.py
+++ Exams/Python-Web-Framework-Retake-Exam-20-August-2023/PizzaGang/PizzaGang/main/filters.py
@@ -1,13 +1,22 @@
 import django_filters
 from django_filters import CharFilter
+from django.forms.widgets import TextInput
 
 from .models import Pizza
 
 
 class PizzaOrderFilter(django_filters.FilterSet):
-    name = CharFilter(field_name='name', lookup_expr='icontains')
-    ingredients = CharFilter(field_name='ingredients', lookup_expr='icontains')
+    name = CharFilter(
+        field_name='name',
+        lookup_expr='icontains',
+        widget=TextInput(attrs={'placeholder': 'Search name'})
+    )
+    ingredients = CharFilter(
+        field_name='ingredients',
+        lookup_expr='icontains',
+        widget=TextInput(attrs={'placeholder': 'Search ingredients'})
+    )
 
     class Meta:
         model = Pizza
-        fields = ('name', 'ingredients', 'price', 'is_special', 'is_offer', 'is_vege')
+        fields = ('name', 'ingredients', 'is_special', 'is_offer', 'is_vege')
