cb7c42438bf0798f35144ec6b98e2277b7437807
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2023-08-25 18:05:53 +0300

    rewriting old exercises

--- main.py
+++ main.py
@@ -1,43 +1,25 @@
-days = int(input()) - 1
-room_type = input()
-review = input()
+month = input()
+nights = int(input())
 
-final_price = 0
+final_ap_price, final_st_price = 0, 0
 
+if month == 'May' or month == 'October':
+    final_ap_price, final_st_price = nights * 65, nights * 50
 
-def apply_discount():
-    global final_price
+    if nights > 14:
+        final_st_price *= 0.70
+    elif nights > 7:
+        final_st_price *= 0.95
+elif month == 'June' or month == 'September':
+    final_ap_price, final_st_price = nights * 68.70, nights * 75.20
 
-    if room_type == 'apartment':
-        if days < 10:
-            final_price *= 0.70
-        elif 10 < days < 15:
-            final_price *= 0.65
-        elif days > 15:
-            final_price *= 0.50
-    elif room_type == 'president apartment':
-        if days < 10:
-            final_price *= 0.90
-        elif 10 < days < 15:
-            final_price *= 0.85
-        elif days > 15:
-            final_price *= 0.80
+    if nights > 14:
+        final_st_price *= 0.80
+elif month == 'July' or month == 'August':
+    final_ap_price, final_st_price = nights * 77, nights * 76
 
+if nights > 14:
+    final_ap_price *= 0.90
 
-if room_type == 'room for one person':
-    final_price += 18 * days
-    apply_discount()
-elif room_type == 'apartment':
-    final_price += 25 * days
-    apply_discount()
-elif room_type == 'president apartment':
-    final_price += 35 * days
-    apply_discount()
-
-
-if review == 'positive':
-    final_price *= 1.25
-elif review == 'negative':
-    final_price *= 0.90
-
-print(f"{final_price:.2f}")
+print(f"Apartment: {final_ap_price:.2f} lv.")
+print(f"Studio: {final_st_price:.2f} lv.")
