43e9e8eba7b23512a6bfd572622fbcdc31cfb4ad
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2023-08-27 23:56:41 +0300

    reminding myself of old things/syntax

--- main.py
+++ main.py
@@ -1,19 +1,61 @@
-cubic_meters = int(input()) * int(input()) * int(input())
-value = 0
-no_more_space = False
+calculations = {
+    'add': lambda x, y: x + y,
+    'subtract': lambda x, y: x - y,
+    'divide': lambda x, y: x / y,
+    'multiply': lambda x, y: x * y
+}
 
-command = input()
+print(calculations['add'](2, 2))
+print(calculations['subtract'](2, 2))
+print(calculations['divide'](2, 2))
+print(calculations['multiply'](2, 2))
 
-while command != 'Done':
-    value += int(command)
+print('---------------------------------------------------------------------------------------')
 
-    if value > cubic_meters:
-        no_more_space = True
-        break
+import time
+import math
 
-    command = input()
 
-if no_more_space:
-    print(f"No more free space! You need {value - cubic_meters} Cubic meters more.")
-else:
-    print(f"{cubic_meters - value} Cubic meters left.")
+def calculate_time(func):
+    def inner1(*args, **kwargs):
+        begin = time.time()
+
+        func(*args, **kwargs)
+
+        end = time.time()
+        print("Total time taken in : ", func.__name__, end - begin)
+
+    return inner1
+
+
+@calculate_time
+def factorial(num):
+    time.sleep(2)
+    print(math.factorial(num))
+
+
+factorial(10)
+
+print('---------------------------------------------------------------------------------------')
+
+print("yes", "no", "yes")
+
+print("yes", end="\n\n")
+print("no", end="\n\n")
+print("yes", end="\n\n")
+
+print('---------------------------------------------------------------------------------------')
+
+
+def make_order(price, *args, **kwargs):
+    print(price, end='; ')
+    [print(item, end=' ') for item in args]
+
+    for key, value in kwargs.items():
+        if key == 'is_active':
+            print(f"\nActive: {value}")
+        elif key == 'is_offer':
+            print(f"\nOffer: {value}")
+
+
+make_order(67, 'Jonathan White - T-shirt', 'Armani - Shorts', 'Rolex g255 - Watch', is_active=True, is_offer=True, teea=True)
