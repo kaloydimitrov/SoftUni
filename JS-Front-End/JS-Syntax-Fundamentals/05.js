5c5768bda6cd3542416c09d0c618faeac26f3ca6
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2023-10-28 15:24:26 +0300

    JS exercises in SoftUni

---
+++ JS-Front-End/JS-Syntax-Fundamentals/05.js
@@ -0,0 +1,22 @@
+function solve(num1, num2, operator) {
+    if (operator == "+") {
+        console.log(num1 + num2);
+    }
+    else if (operator == "-") {
+        console.log(num1 - num2);
+    }
+    else if (operator == "*") {
+        console.log(num1 * num2);
+    }
+    else if (operator == "**") {
+        console.log(num1 ** num2);
+    }
+    else if (operator == "%") {
+        console.log(num1 % num2);
+    }
+    else if (operator == "/") {
+        console.log(num1 / num2);
+    }
+}
+
+solve(2, 3, "+");
